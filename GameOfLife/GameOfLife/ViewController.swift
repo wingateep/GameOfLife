fef9bd22aab65bebfd3de45bde0ea72e2badf532
elizabeth paige <wingateep@Icloud.com>
2020-07-27 13:02:14 -0400

    made project

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Elizabeth Wingate on 7/27/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
