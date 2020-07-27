8ca5c3c21d7cdc28496490d54706172a9a46bc53
elizabeth paige <wingateep@Icloud.com>
2020-07-27 14:34:30 -0400

    rough set up of storyboard + added a few button images

---
+++ GameOfLife/GameOfLife/View Controllers/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  GameOfLife
+//
+//  Created by Elizabeth Wingate on 7/27/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
